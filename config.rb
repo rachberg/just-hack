0e80533a4e73747a3813d37d8ef1c359a34e8e64
Rachel Bergmann <rachelbergmann@gmail.com>
2015-05-10 16:46:27 +0200

    added sass and compass to the project

---
+++ config.rb
@@ -0,0 +1,17 @@
+# Compass CSS framework config file
+
+# require 'css-parser'
+# Require any additional compass plugins here.
+
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir = "css/scss"
+css_dir = "css"
+images_dir = "images"
+fonts_dir = "fonts"
+javascripts_dir = "js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
