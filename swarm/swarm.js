2588f839598f7293dc4f18177bf35f65f3c09140
Amedee d'Aboville <amedee.daboville@gmail.com>
2015-05-21 14:33:04 +0200

    New fps counting, removing quadtree because it's not the bottleneck

--- swarm/swarm.js
+++ swarm/swarm.js
@@ -1,12 +1,10 @@
-//var quadtree = d3.geom.quadtree()
-//                .extent([[-1, -1], [width + 1, height + 1]])
-//                (data);
 
 var data = [];
 var link_array = [];
-var numPoints = 200;
+var numPoints = 150;
 var link_dist = 0.01;
 var speed = 0.00005
+//var k_closest = 10;
 var colors = ["rgb(255, 48, 124)",
               "rgb(189, 34, 92)",
               "rgb(0, 219, 187)",
@@ -27,13 +25,13 @@ var color = d3.scale.linear()
 
 var numColors = colors.length;
 for (var i = 0; i < numPoints; i++) {
-    data.push({xloc: Math.random(), yloc: Math.random() , xvel: speed*(Math.random() - 0.5), yvel: speed * (Math.random() - 0.5)});
+    data.push({xloc: Math.random(), yloc: Math.random() , xvel: speed*(Math.random() - 0.5), yvel: speed * (Math.random() - 0.5), idx: i});
 }
+
 
 function euclidDistance(ax, ay, bx, by){
     return (Math.pow(ax-bx, 2) + Math.pow(ay-by, 2));
 }
-
 
 function updateLinks() {
     link_array = [];
@@ -42,12 +40,32 @@ function updateLinks() {
             var dist = euclidDistance(data[i].xloc, data[i].yloc, data[j].xloc, data[j].yloc);
             if (dist < link_dist) {
                     var yloc1 = data[i].yloc;
+                    //link_array.push([i,j, 1 - dist/link_dist, colors[Math.floor(yloc1 * colors.length)]])
                     link_array.push([i,j, 1 - dist/link_dist, color(yloc1)]);
                     //if (i == 1) {console.log(color(yloc1))};
                 }
             }
         }
     }
+//function quadTreelinks() {
+//    quadtree = d3.geom.quadtree()
+//                 .extent([[-1, -1], [w + 1, h + 1]])
+//                 .x(function (d) { return d.xloc})
+//                 .y(function (d) { return d.yloc})
+//                 (data)
+//    link_array = [];
+//    for (var i=0; i< numPoints;i++) {
+//        var bestqueue = new Array(quadtree);
+//        var result = [];
+//        knearest(bestqueue, result, data[i].xloc, data[i].yloc, k_closest);
+//        for (j = 0; j < k_closest; j++)
+//            var neighbor = result[j].point;
+//            var dist = euclidDistance(data[i].xloc, data[i].yloc, neighbor.xloc, neighbor.yloc);
+//            if (dist < link_dist) {
+//                link_array.push([i, neighbor.idx, 1 - (dist/link_dist), color(data[i].yloc)]);
+//            }
+//    }
+//}
 updateLinks()
 var w = window.innerWidth,
     h = window.innerHeight,
@@ -57,9 +75,14 @@ var w = window.innerWidth,
         .range([0, w]),
     y = d3.scale.linear()
         .domain([0, 1])
-        .range([0, h]);
+        .range([0, h]),
+    quadtree = d3.geom.quadtree()
+                 .extent([[-1, -1], [w + 1, h + 1]])
+                 .x(function (d) { return d.xloc})
+                 .y(function (d) { return d.yloc})
+                 (data)
 
-    var latest = new Date(),
+    var latest = performance.now()
     last = latest,
     fps = [];
 
@@ -82,6 +105,7 @@ chart.selectAll("circle")
         return y(d.yloc); 
     })
     .attr("r", 1.5)
+    .style("stroke", "grey");
 
 var path = chart.selectAll("line")
     .data(link_array)
@@ -107,11 +131,12 @@ function motion(e, index, array) {
     if (e.yloc > 1) { e.yloc = 1;  e.yvel = -1 * e.yvel}
     if (e.yloc < 0) { e.yloc = 0;  e.yvel = -1 * e.yvel}
 }
-
-d3.timer(function() {
-
+var count = 0;
+d3.timer(function(elapsed) {
+    count++;
     data.forEach(motion);
-    if (Math.random() > 0.8) { updateLinks()}
+    //quadTreelinks();
+    updateLinks();
 
     chart.selectAll("circle")
         .attr("cx", function(d) { return x(d.xloc); })
@@ -126,10 +151,11 @@ d3.timer(function() {
         .attr("opacity", function(d) { return d[2]})
         .style("stroke", function(d) { return d[3]})
 
-    latest = new Date();
+    latest = performance.now()
     fps.push(1/(latest-last)*1000);
     fps.shift();
     last = latest;
-    d3.select("#fps span").text(Math.round(d3.mean(fps)));
+
+    d3.select("#fps span").text(Math.floor((count/elapsed)*1000));
     d3.select("#links span").text(link_array.length);
 });
